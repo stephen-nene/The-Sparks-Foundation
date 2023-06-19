73ae6876fbacc3c8f9f3331313f1b5661a025267
stephen nene <stevekid705@gmail.com>
2023-06-19 11:40:56 +0300

    added fb

--- oath/src/App.jsx
+++ oath/src/App.jsx
@@ -1,77 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Home';
-import { ClerkProvider, SignedIn, SignedOut, SignIn, SignUp, RedirectToSignIn, } from "@clerk/clerk-react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { dark } from '@clerk/themes';
+
 import PublicPage from './Public';
 import { Button } from 'antd';
 
-if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
+export default  function App() {
 
-function App() {
-
-  const navigate = useNavigate();
 
   return (
-    <ClerkProvider 
-         appearance={{
-      // baseTheme: dark
-    }}
-      navigate={(to) => navigate(to)}
-      publishableKey={clerkPubKey}>
-      <Routes>
-        <Route
-          path="/"
-          element={<PublicPage />} />
-      <Route
-        path="/sign-in"
-        element={
-          // <PublicPage/>
-          <div className="sign-page">
-            <SignIn routing="path" path="/sign-in" />
-            <div className="button-container">
-              <Button className="home-button" href="/">
-                Public
-              </Button>
-            </div>
-          </div>
-        }
-      />
-      <Route
-        path="/sign-up"
-        element={
-          <div className="sign-page">
-            <SignUp routing="path" path="/sign-up" />
-            <div className="button-container">
-              <Button className="home-button" href="/">
-              Public
-              </Button>
-            </div>
-          </div>
-        }
-        />
-        <Route
-          path='/dashboard'
-          element={
-            <>
-              <SignedIn>
-                <Home />
-              </SignedIn>
-              <SignedOut>
-                <RedirectToSignIn />
-              </SignedOut>
-            </>
-          }
-        />
-      </Routes>
+    <>
+    <div className="whole">
+    <Home/>
+    </div>
 
-    </ClerkProvider>
+    </>
+
   );
 }
 
-export default App
+
--- oath/src/Home.jsx
+++ oath/src/Home.jsx
@@ -1,39 +1,76 @@
-import React from "react";
-import { message } from "antd";
-import { UserButton, useUser } from "@clerk/clerk-react";
+import React from 'react';
+import { MDBBtn, MDBContainer, MDBSpinner, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from 'mdb-react-ui-kit';
+import { message } from 'antd';
+import FacebookLogin from 'react-facebook-login';
+import './App.css'; // Import the custom CSS file
 
-export default function Home() {
-    const { isLoaded, isSignedIn, user } = useUser();
+function App() {
+  const [loading, setLoading] = React.useState(false);
 
-    if (!isLoaded || !isSignedIn) {
-        message.info("Something is wrong");
-        return null;
+  const handleClick = (name) => {
+    message.success(name + 'oath coming soon');
+  };
+
+  const handleFacebook = (response) => {
+    if (response.email) {
+      message.success(`Logged in with Facebook: ${response.email}`);
+    } else {
+      message.error('Facebook login failed');
     }
+  };
 
-    const handleEmailCopy = () => {
-        navigator.clipboard.writeText(emailAddress);
-        message.success("Email copied to clipboard");
-      };
+  return (
+    <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
+      <MDBRow>
+        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+          <h1 className='my-5 display-3 fw-bold ls-tight px-3' style={{ color: 'hsl(218, 81%, 95%)' }}>
+            STeve NEne <br />
+            <span style={{ color: 'hsl(218, 81%, 75%)' }}>oAth app</span>
+          </h1>
+          <p className='px-3' style={{ color: 'hsl(218, 81%, 85%)' }}>
+            description goes here
+          </p>
+        </MDBCol>
 
-    const { emailAddress } = user.emailAddresses[0];
-
-    return (
-        <>
-            <nav>
-                <div className="nav-container">
-                    <UserButton className="user-icon" />
-                </div>
-            </nav>
-            <p>Hello, {user.firstName}, you are signed in to Steve's Spark Foundation project</p>
-            <p>
-        Your email is{" "}
-        <a
-          onClick={handleEmailCopy}
-          style={{ cursor: "pointer" }}
-        >
-          {emailAddress}
-        </a>
-      </p>
-        </>
-    );
+        <MDBCol md='6' className='position-relative'>
+          <MDBCard className='my-5 bg-glass'>
+            <MDBCardBody className='p-5'>
+              <div className='social-buttons text-center gap-2 d-flex flex-column justify-content-center'>
+                <p>Sign up with:</p>
+                {loading ? (
+                  <MDBBtn disabled>
+                    <MDBSpinner size='sm' role='status' tag='span' className='me-2' />
+                    Loading...
+                  </MDBBtn>
+                ) : (
+                  <FacebookLogin
+                    appId="728148255982743"
+                    autoLoad={false}
+                    fields="name,email,picture"
+                    callback={handleFacebook}
+                    render={(renderProps) => (
+                      <MDBBtn tag='a' color='secondary' className='mx-3 facebook' onClick={renderProps.onClick}>
+                        <MDBIcon fab icon='facebook-f' size='sm' /> Facebook
+                      </MDBBtn>
+                    )}
+                  />
+                )}
+                <MDBBtn tag='a' color='secondary' className='mx-3 google'>
+                  <MDBIcon fab icon='google' size='sm' /> Google
+                </MDBBtn>
+                <MDBBtn tag='a' color='secondary' className='mx-3 twitter' onClick={() => handleClick('twitter')}>
+                  <MDBIcon fab icon='twitter' size='sm' /> Twitter
+                </MDBBtn>
+                <MDBBtn tag='a' color='secondary' className='mx-3 github' onClick={() => handleClick('github')}>
+                  <MDBIcon fab icon='github' size='sm' /> Github
+                </MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
+
+export default App;
